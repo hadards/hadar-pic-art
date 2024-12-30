0b1a50a3e77ee2e1e77a6d8133a76bba245b2878
Hadar <hadards@yahoo.com>
2024-12-30 21:12:02 +0200

    add picture

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ImageCarouselComponent } from '../image-carousel/image-carousel.component';
-import { ImageGalleryComponent } from '../image-gallery/image-gallery.component';
 import { IImage } from '../models/image.model';
 
 @Component({
@@ -14,32 +11,37 @@ export class LandingComponent {
     {
       url: 'assets/images/Eitan cartoon 4.jpg',
       alt: 'AI Generated Artwork 1',
-      title: 'Abstract Dreams'
+      title: 'Cartoon Figures'
     },
     {
       url: 'assets/images/2.png',
-      alt: 'AI Generated Artwork 3',
-      title: 'Future Visions'
+      alt: 'AI Generated Artwork 2',
+      title: 'Abstract Dreams'
     },
     {
       url: 'assets/images/hadar.jpg',
-      alt: 'AI Generated Artwork 2',
+      alt: 'AI Generated Artwork 3',
       title: 'Digital Landscapes'
     },
     {
       url: 'assets/images/4.jpg',
-      alt: 'AI Generated Artwork 2',
-      title: 'Digital Landscapes'
+      alt: 'AI Generated Artwork 4',
+      title: 'Future Visions'
     },
     {
       url: 'assets/images/eitan-invite.jpg',
-      alt: 'AI Generated Artwork 2',
+      alt: 'AI Generated Artwork 5',
       title: 'Digital Landscapes'
     },
     {
       url: 'assets/images/1.png',
-      alt: 'AI Generated Artwork 3',
+      alt: 'AI Generated Artwork 6',
       title: 'Future Visions'
+    },
+    {
+      url: 'assets/images/dashmaries.JPEG',
+      alt: 'AI Generated Artwork 7',
+      title: 'Funny Cartoons'
     }
   ];
 }
